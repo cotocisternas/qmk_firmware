69a919bb3e72aa4d5fadf66323e48b91a536791b
Coto Cisternas <coto@petabyte.cl>
2026-07-24 17:27:15 -0400

    feat: move space

--- keyboards/sofle/keymaps/vincent/keymap.c
+++ keyboards/sofle/keymaps/vincent/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  Mute |    |  Play |------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | Entr |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | GUI  | Alt  | Uppr | Lowr | / Bspc  /       \ Space\ | Uppr | PgUp | PgDwn | Del |
+ *            | GUI  | Alt  | Uppr | Lowr | / Space /       \ Space\ | Uppr | PgUp | PgDwn | Del |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                      KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR, KC_BSPC,  KC_SPC,  TL_UPPR, KC_PGDN,  KC_PGUP, KC_DEL
+                      KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR, KC_SPC,  KC_SPC,  TL_UPPR, KC_PGDN,  KC_PGUP, KC_BSPC
 ),
 
 /*
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,                    _______, KC_PRVWD, _______, KC_NXTWD, KC_PSCR, _______,
     KC_CAPS,  _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
     _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
-                      _______, _______, _______, _______, _______,  _______, _______, KC_LSTRT, KC_LEND, KC_INS
+                      _______, _______, _______, _______, _______,  _______, _______, KC_LSTRT, KC_LEND, KC_DEL
 ),
 
 /*
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      | Undo |  Cut | Copy | Pste |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /      /        \      \ |      | Home  | End | Ins  |
+ *            |      |      |      |      | /      /        \      \ |      | Home  | End | Del  |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
